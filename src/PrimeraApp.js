29089c7afce5c6f67d45dab43b876b48548e1f8d
TofuBoy <grandfunk30@gmail.com>
2021-05-29 16:31:58 -0500

    04-Use-Variables

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -4,16 +4,22 @@ import React from 'react';
 
 const PrimeraApp = ()=>{
 
-    
+    const objeto={
+        nombre: "Pedro Arce",
+        edad: 28
+    };
+
+    const saludo="Hola Mundo"
 
     return  (
    /* <Fragment>
         <h1>Hola Mundo</h1>
         <p>Primer Parrafo</p>
     </Fragment>
-    */
+   */
     <>
-        <h1>Hola Mundo</h1>
+        <h1>{saludo}</h1>
+        <pre>{JSON.stringify(objeto,null,3)}</pre>
         <p>Primer Parrafo</p>
     </>
     );
