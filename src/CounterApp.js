afb07c439f59fbd5a58ff5e9987b2f86d0790829
TofuBoy <grandfunk30@gmail.com>
2021-06-14 15:48:46 -0500

    useState

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp=({value})=>{
 
+    //const state=useState('Goku');
+    //console.log(state)
     //handleAdd
-    const handleAdd=(e)=>{
-        console.log(e)
+    const [counter, setCounter]=useState(0);
+    const handleAdd=()=>{
+        //setCounter(counter+1)
+        setCounter((c)=>c+1)
     }
     return(
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
+            <h2>{counter}</h2>
             <button onClick={handleAdd}> +1 </button>
 
         </>
