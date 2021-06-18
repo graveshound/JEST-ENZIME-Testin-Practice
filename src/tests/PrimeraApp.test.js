f29071e827540f3f744fa3667bf48eb3de21e885
TofuBoy <grandfunk30@gmail.com>
2021-06-18 11:56:09 -0500

    EnzimeTesting

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,15 +1,28 @@
-import { render } from "@testing-library/react"
+//import { render } from "@testing-library/react"
 import PrimeraApp from "../PrimeraApp"
 import React  from 'react';
+import {shallow} from 'enzyme'
+import '@testing-library/jest-dom'
 
 describe('Prueba de PrimeraAPP', () => {
-    test('debe de mostrar el mensaje "Hola, soy Goku"', () => {
+    //test('debe de mostrar el mensaje "Hola, soy Goku"', () => {
         
+        //const saludo="Hola Soy Goku"
+
+        //const { getByText }= render( <PrimeraApp saludo2={saludo} />)
+
+       // expect( getByText(saludo) ).toBeInTheDocument();
+   // })
+
+   test('Debe Renderizar correctamente <PrimeraApp></PrimeraApp>', () => {
+       
         const saludo="Hola Soy Goku"
+        const wrapper=shallow(<PrimeraApp saludo2={ saludo }/>)
 
-        const { getByText }= render( <PrimeraApp saludo2={saludo} />)
+        expect(wrapper).toMatchSnapshot();
 
-        expect( getByText(saludo) ).toBeInTheDocument();
-    })
+
+   })
+   
     
 })
